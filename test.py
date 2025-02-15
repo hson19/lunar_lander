e56992c1226bf3bca1e288f5b0c82a074e6efd44
hson19 <hadriensonnet73@gmail.com>
2025-02-15 17:12:33 +0100

    changing code organisation

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 import gymnasium as gym
 import numpy as np
+from training_udrl.training import DecisionTransformerBehavior
+import torch
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Initialise the environment
 env = gym.make("LunarLander-v3", render_mode="human")
 
 # Reset the environment to generate the first observation
 observation, info = env.reset(seed=42)
+print(observation)
+action_size=4
+state_size = 8
+model = DecisionTransformerBehavior(2,state_size,action_size,512,10,1).load("models/training_finished").to(device)
+# DecisionTransformerBehavior(2,env.action_space,,512,10,1).to(device)
+command = [1000,100]
 for _ in range(1000):
     # this is where you would insert your policy
     # action = env.action_space.sample()
